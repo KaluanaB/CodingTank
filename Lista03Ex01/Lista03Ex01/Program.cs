8a13ad075c7d54dabbf206a2f9d1906193c1f437
Kaluanã Mirim <132582320+kmirim@users.noreply.github.com>
2023-10-10 18:49:48 -0300

    Add files via upload

---
+++ Lista03Ex01/Lista03Ex01/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Lista03Ex01
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            int [] A = { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 };
+            int[] B = { 12, 14, 15, 17, 19, 21, 23, 25, 27, 29 };
+            Console.WriteLine($"Os valores em A são:");
+            foreach (int valor in A)
+            {
+                Console.Write(valor + " ");
+            }
+            Console.WriteLine();
+            
+            Console.WriteLine($"O valores em B são:");
+            foreach (int valor in B)
+            {
+                Console.Write(valor + " ");
+            }
+            Console.WriteLine();
+
+            int[] C = new int[10];
+
+            for (int i = 0; i < 10; i++)
+            {
+                C[i] = A[i] + B[9 - i];
+            }
+            Console.WriteLine($"Os valores em C são:");
+            foreach (var valor in  C)
+            {
+                Console.Write(valor + " ");
+            }
+        }
+    }
+}
---
+++ Lista03Ex02/Lista03Ex02/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Lista03Ex02
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Informe 10 (dez) números inteiros separado por espaços:");
+            
+            
+        }
+    }
+}
